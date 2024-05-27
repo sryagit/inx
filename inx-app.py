d05bf5b09fa816eed99d98ce57ffd1f66b65fcc8
sryagit <158165193+sryagit@users.noreply.github.com>
2024-05-27 16:27:33 +0530

    Update inx-app.py

--- inx-app.py
+++ inx-app.py
@@ -3,6 +3,9 @@ import pandas as pd
 import pickle
 
 st.title('Employee Performance web app')
+st.write("""### h3""")
+st.write("""## h3""")
+st.write("""# h3""")
 
 department = st.selectbox('Select your department', ['HR', 'R&D', 'Legal'])
 
