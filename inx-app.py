65690a1560fc2454513658b7e98a8264966fc1d6
sryagit <158165193+sryagit@users.noreply.github.com>
2024-05-27 16:30:03 +0530

    Update inx-app.py

--- inx-app.py
+++ inx-app.py
@@ -10,3 +10,5 @@ st.write("# heading 1")
 department = st.selectbox('Select your department', ['HR', 'R&D', 'Legal'])
 
 no_of_trainings = st.slider('Number of Trainings', 1, 5, 1)
+
+st.button('Predict Performance')
