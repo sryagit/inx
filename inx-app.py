6fe86f00eaf68e25894be2e3078373e48008da88
sryagit <158165193+sryagit@users.noreply.github.com>
2024-05-27 16:31:09 +0530

    Update inx-app.py

--- inx-app.py
+++ inx-app.py
@@ -11,4 +11,7 @@ department = st.selectbox('Select your department', ['HR', 'R&D', 'Legal'])
 
 no_of_trainings = st.slider('Number of Trainings', 1, 5, 1)
 
+gender = st.radio('Gender', ['Male', 'Female'])
+
 st.button('Predict Performance')
+
