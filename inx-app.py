b331ebc927955013a8710eab27cd307709fbbc73
sryagit <158165193+sryagit@users.noreply.github.com>
2024-05-27 16:25:41 +0530

    Update inx-app.py

--- inx-app.py
+++ inx-app.py
@@ -5,3 +5,6 @@ import pickle
 st.title('Employee Performance web app')
 
 department = st.selectbox('Select your department', ['HR', 'R&D', 'Legal'])
+
+st.write("""### Number of Trainigs""")
+no_of_trainings = st.slider('Number of Trainings', 1, 5, 1)
