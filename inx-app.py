c44abf89f18a82e06d5f3b0e5e5baad3e9b2b546
sryagit <158165193+sryagit@users.noreply.github.com>
2024-05-27 16:26:27 +0530

    Update inx-app.py

--- inx-app.py
+++ inx-app.py
@@ -6,5 +6,4 @@ st.title('Employee Performance web app')
 
 department = st.selectbox('Select your department', ['HR', 'R&D', 'Legal'])
 
-st.write("""### Number of Trainigs""")
 no_of_trainings = st.slider('Number of Trainings', 1, 5, 1)
